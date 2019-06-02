2ed75484bd26abc77d4074dbc1f3396f591328cf
Benjamin Cronce <bcronce@gmail.com>
2019-06-02 11:45:58 -0500

    Helper function to CAS enums

---
+++ BoringConcurrency/Common/InterlockedEnum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace BoringConcurrency.Common
+{
+    public static class InterlockedEnum
+    {
+        private static void AssertInt<T>() where T : Enum
+        {
+            Type typeOfEnum = Enum.GetUnderlyingType(typeof(T));
+            bool isIntOrLong = typeOfEnum == typeof(int);
+            Debug.Assert(isIntOrLong, "Only supports int");
+        }
+
+        public static T CompareExchange<T>(ref T location1, T value, T comparand) where T : Enum
+        {
+#if DEBUG
+            AssertInt<T>();
+#endif
+
+            int returnValue = Interlocked.CompareExchange(
+                location1: ref Unsafe.As<T, int>(ref location1)
+                , value: Unsafe.As<T, int>(ref value)
+                , comparand: Unsafe.As<T, int>(ref comparand)
+            );
+            return Unsafe.As<int, T>(ref returnValue);
+        }
+
+        public static T Exchange<T>(ref T location1, T value) where T : Enum
+        {
+#if DEBUG
+            AssertInt<T>();
+#endif
+
+            int returnValue = Interlocked.Exchange(
+                location1: ref Unsafe.As<T, int>(ref location1)
+                , value: Unsafe.As<T, int>(ref value)
+            );
+            return Unsafe.As<int, T>(ref returnValue);
+        }
+    }
+}
